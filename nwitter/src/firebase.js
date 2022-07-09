dff6aae7887bee9c074fd99608b4170947e4549b
Min <kjj9500@gmail.com>
2022-07-09 22:59:23 +0900

    firebase import 변경

--- nwitter/src/firebase.js
+++ nwitter/src/firebase.js
@@ -1,4 +1,7 @@
-import * as firebase from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCX9xVu28DlJMSLMDR1XurPnObF_pYgJgk",
