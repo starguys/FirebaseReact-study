bd0be238b438e757b3b27ac6bc8e77888e8c2a45
Min <kjj9500@gmail.com>
2022-07-10 01:19:31 +0900

    firebase config .env로 변경

--- nwitter/src/firebase.js
+++ nwitter/src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX9xVu28DlJMSLMDR1XurPnObF_pYgJgk",
-  authDomain: "nwitter-872d1.firebaseapp.com",
-  projectId: "nwitter-872d1",
-  storageBucket: "nwitter-872d1.appspot.com",
-  messagingSenderId: "570003902939",
-  appId: "1:570003902939:web:df558d1e605855e300d7fe",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
