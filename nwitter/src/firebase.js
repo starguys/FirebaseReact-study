ed6e02c08b25304b301d0f01b0d5e63f559c1b34
Min <kjj9500@gmail.com>
2022-07-09 18:12:15 +0900

    firebase init

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCX9xVu28DlJMSLMDR1XurPnObF_pYgJgk",
+  authDomain: "nwitter-872d1.firebaseapp.com",
+  projectId: "nwitter-872d1",
+  storageBucket: "nwitter-872d1.appspot.com",
+  messagingSenderId: "570003902939",
+  appId: "1:570003902939:web:df558d1e605855e300d7fe",
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
